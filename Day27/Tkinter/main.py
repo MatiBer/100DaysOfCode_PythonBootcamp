6a1d2f962aec9a041ba13145d5ccbb0a1b87629f
MBerlin <you@example.com>
2023-09-29 22:16:09 +0200

    Button, Entry, Setting component options

--- Day27/Tkinter/main.py
+++ Day27/Tkinter/main.py
@@ -1,15 +1,31 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-#Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack(side="bottom")
 
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
 
+# Button
 
+def button_clicked():
+    print(input.get())
+    my_label.config(text=input.get())
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 window.mainloop()
