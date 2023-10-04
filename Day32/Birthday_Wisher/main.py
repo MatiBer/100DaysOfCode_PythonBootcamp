367861bc254aa6ff02337d9a2fd4043e386dfb8c
Mateusz Berliński <01158402@pw.edu.pl>
2023-10-04 23:54:48 +0200

    Automated Birthday Wisher

---
+++ Day32/Birthday_Wisher/main.py
@@ -0,0 +1,44 @@
+import datetime as dt
+import smtplib
+import random
+import pandas
+
+my_email = "mateusz.berlinski7@gmail.com"
+password = ""
+
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+today = dt.datetime.now()
+
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 4. Send the letter generated in step 3 to that person's email address.
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
+
+
+
+
+
+
