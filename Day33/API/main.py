2de25a7be54ee7dc3e7d8db86606c056252e9c50
Mateusz Berliński <01158402@pw.edu.pl>
2023-10-06 00:09:53 +0200

    ISS Overhead Notifier App

--- Day33/API/main.py
+++ Day33/API/main.py
@@ -1,14 +1,53 @@
 import requests
+from datetime import datetime
+import smtplib
+import time
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+MY_LAT = 52.229675
+MY_LONG = 21.012230
 
-data = response.json()
+my_email = "mateusz.berlinski7@gmail.com"
+password = ""
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
 
-iss_position = (longitude, latitude)
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-print(iss_position)
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if abs(iss_latitude - MY_LAT) < 5 and abs(iss_longitude - MY_LONG) < 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    if time_now.hour <= sunrise or time_now.hour >= sunset:
+        return True
+
+
+while True:
+    time.sleep(60)
+    # Then send me an email to tell me to look up.
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="mateusz.berlinski7@yahoo.com",
+                                msg="Subject:ISS is Overhead! \n\nLook up ISS is Overhead!")
